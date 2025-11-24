73e3a3d3c9c68aa30f22bb2cec30eb98e92d3cdb
Ana Dias <anitacdf756@gmail.com>
2025-11-24 14:45:18 -0300

    Update reference link in References component

--- frontend-web/src/components/References.tsx
+++ frontend-web/src/components/References.tsx
@@ -6,7 +6,7 @@ const References: React.FC = () => {
       <div className="references-title">Fontes:</div>
       <ul className="references-list">
         <li className="reference-item">
-          ROSENFELD, Luiz Gastão et al. Valores de referência para exames laboratoriais de hemograma da população adulta brasileira: Pesquisa Nacional de Saúde. Revista Brasileira de Epidemiologia, v. 22, p. art. e190003 [13], 2019. Tradução. Disponível em: <a href="https://doi.org/10.1590/1980-549720190003.supl.2" target="_blank" rel="noopener noreferrer">https://doi.org/10.1590/1980-549720190003.supl.2</a>. Acesso em: 14 out. 2025.
+          ROSENFELD, Luiz Gastão et al. Valores de referência para exames laboratoriais de hemograma da população adulta brasileira: Pesquisa Nacional de Saúde. Revista Brasileira de Epidemiologia, v. 22, p. art. e190003 [13], 2019. Tradução. Disponível em: <a href="https://www.scielo.br/j/rbepid/a/79JFJqJnBqcpgFL4CHVGdxS/?lang=pt" target="_blank" rel="noopener noreferrer">https://doi.org/10.1590/1980-549720190003.supl.2</a>. Acesso em: 14 out. 2025.
         </li>
       </ul>
     </div>
